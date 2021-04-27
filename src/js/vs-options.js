98ddcedc502c652fd0d7d7c750bed8d5123b38c3
Zhoraev D <newlifenewgoal@gmail.com>
2021-04-27 14:37:00 +0600

    options: refactoring key name

--- src/js/vs-options.js
+++ src/js/vs-options.js
@@ -1,43 +1,40 @@
-class VSOptions{
+class VSOptions {
 
-    constructor(sliderID, options){
+    constructor(sliderID, options) {
 
         this.options = {
             sliderID: sliderID,
-            sliderClass:'vs-slider',
-            slideClass:'vs-slide',
-            autoplay:true,
-            timer:2000,
-            idx:0,
-            infinty:true,
-            isCtrlEnable:true,
-            animation:{
-                name:"vs-default",
-                durition:2
+            sliderClass: 'vs-slider',
+            slideClass: 'vs-slide',
+            autoplay: true,
+            timer: 2000,
+            idx: 0,
+            infinty: true,
+            controlEnable: true,
+            dotsEnable: true,
+            animation: {
+                name: "vs-default",
+                durition: 2
             }
         };
 
-        if(options !== null && options !== undefined) this.setOptions(options);
+        if (options !== null && options !== undefined) this.setOptions(options);
     }
 
-    setOptions(options){
-            //l(options)
-            const optMap = new Map(Object.entries(options));
+    setOptions(options) {
+        //l(options)
+        const optMap = new Map(Object.entries(options));
 
-            optMap.forEach((v, k)=> {
-            
-                if(k !==  undefined && k !== null){
-                   this.options[k] = v;
-                   //l(v)
-                //    l(this.options)
-                }
+        optMap.forEach((v, k) => {
 
-            });
-            //l(this.options);
+            if (k !== undefined && k !== null) {
+                this.options[k] = v;
+            }
+
+        });
     }
 
-    getOptions(){ return this.options; }
-
+    getOptions() { return this.options; }
 }
 
-export default(VSOptions);
+export default VSOptions;
